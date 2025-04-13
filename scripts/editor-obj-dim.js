564f73c384e025c2916a5d5ab9b611b1a92a4594
A2Fais <aseeralfaisal@gmail.com>
2025-04-13 20:22:09 +0600

    removed unnecessary comments

--- scripts/editor-obj-dim.js
+++ scripts/editor-obj-dim.js
@@ -1,16 +1,13 @@
 function CanvasGuides(canvas) {
   const l = console.log.bind(this);
   
-  // Check if device is mobile
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   const guideStrokeWidth = isMobile ? 2 : 0.5;
 
-  // Create new id
   function createNewId() {
     return Math.random().toString(36).substr(2, 9);
   }
 
-  // Add positioning line
   function showPositioningLine(type, point, props) {
     let name = point + type;
     let line = canvas._objects.find((obj) => obj.name === name),
@@ -55,17 +52,14 @@ function CanvasGuides(canvas) {
     });
     canvas.add(line);
   }
-  // Hide all positioning line
+
   function hideAllPositioningLines() {
-    // Get positionlines objects ids in array
     let positionlines = canvas._objects.filter((obj) => obj.isPositioningLine);
-    // Hide all positionlines
     for (let i = 0; i < positionlines.length; i++) {
       canvas.remove(positionlines[i]);
     }
   }
 
-  // Hide positioning line
   function hidePositioningLine(type, point) {
     let name = point + type;
     let line = canvas._objects.find((obj) => obj.name === name);
@@ -73,10 +67,9 @@ function CanvasGuides(canvas) {
       line.set({
         opacity: 0,
       });
-      // canvas.renderAll();
     }
   }
-  // Get positioning points of object
+
   function getPositioningPoints(obj) {
     let width = obj.getScaledWidth(),
       height = obj.getScaledHeight(),
@@ -85,11 +78,9 @@ function CanvasGuides(canvas) {
       pointsX = [left, left + width / 2, left + width],
       pointsY = [top, top + height / 2, top + height];
 
-    // make points integer
     pointsX = pointsX.map((point) => Math.round(point));
     pointsY = pointsY.map((point) => Math.round(point));
 
-    // Add y to x points
     let pointsXFull = [];
     pointsX.forEach((point, i) => {
       pointsXFull.push({
@@ -97,7 +88,7 @@ function CanvasGuides(canvas) {
         p2: pointsY[0],
       });
     });
-    // Add x to y points
+
     let pointsYFull = [];
     pointsY.forEach((point, i) => {
       pointsYFull.push({
@@ -110,13 +101,13 @@ function CanvasGuides(canvas) {
       y: pointsYFull,
     };
   }
-  // Get Canvas positioning points
+
   function getCanvasPositioningPoints() {
     let width = canvas.getWidth(),
       height = canvas.getHeight(),
       pointsX = [0, width / 2, width],
       pointsY = [0, height / 2, height];
-    // Add p2 to pointsX and pointsY
+    
     let pointsXFull = pointsX.map((point) => {
       return {
         point,
@@ -137,10 +128,9 @@ function CanvasGuides(canvas) {
       y: pointsYFull,
     };
   }
-  // Get canvas object by id
+
   function getObjById(id) {
     let targetObj = null;
-    // let obj = canvas._objects.find(obj => obj.id == id);
     let targetObject = null;
     canvas._objects.forEach((obj) => {
       if (targetObject) return true;
@@ -155,25 +145,20 @@ function CanvasGuides(canvas) {
     });
     return targetObject;
   }
-  // Get positioning points of all objects
+
   function getAllPositioningPoints() {
     let pointsX = [],
       pointsY = [],
       canvasPoints = getCanvasPositioningPoints(),
-      // pointsX = [canvas.getWidth() / 2],
-      // pointsY = [canvas.getHeight() / 2],
       activeObj = canvas.getActiveObject(),
       activeObjId = activeObj?.id;
-    // concat canvas points to pointsX and pointsY
     pointsX = pointsX.concat(canvasPoints.x);
     pointsY = pointsY.concat(canvasPoints.y);
-    // Check if is inside group
+
     if (isObjInGroup(activeObjId)) {
-      // select group
       if (activeObj.group) activeObjId = activeObj.group.id;
     }
 
-    // Check if obj is inside a group
     function isObjInGroup(id) {
       let obj = getObjById(id);
       if (!obj) return false;
@@ -196,7 +181,6 @@ function CanvasGuides(canvas) {
     };
   }
 
-  // Check is object is on points of positioning
   function isObjectOnPoint(objPoints, points, type) {
     let data = {
       onPoint: false,
@@ -222,7 +206,6 @@ function CanvasGuides(canvas) {
     return data;
   }
   let isObjMoved = false;
-  // Show positioning points of object on moving canvas
   canvas.on("object:moving", function (e) {
     isObjMoved = true;
     let obj = e.target;
@@ -235,9 +218,7 @@ function CanvasGuides(canvas) {
       height = obj.getScaledHeight(),
       objPoints = getPositioningPoints(obj),
       points = getAllPositioningPoints(obj);
-    // Check if on x points
     let onXPoints = isObjectOnPoint(objPoints.x, points.x, "Y");
-    // console.log(onXPoints);
     if (onXPoints.onPoint) {
       let point = onXPoints.point,
         leftPos = point;
@@ -252,7 +233,6 @@ function CanvasGuides(canvas) {
         hidePositioningLine("Y", point);
       });
     }
-    // Check if on y points
     let onYPoints = isObjectOnPoint(objPoints.y, points.y, "X");
     if (onYPoints.onPoint) {
       let point = onYPoints.point,
@@ -269,6 +249,7 @@ function CanvasGuides(canvas) {
     }
     canvas.renderAll();
   });
+
   canvas.on("mouse:up", function (e) {
     hideAllPositioningLines();
 
@@ -280,6 +261,7 @@ function CanvasGuides(canvas) {
       if (obj.type == "curved-text") obj.refreshCtx(true);
     }
   });
+
   canvas.on("object:scaling", function (e) {
     let obj = e.target;
     if (obj.type == "curved-text") {
@@ -288,7 +270,7 @@ function CanvasGuides(canvas) {
       obj._updateObj("scaleY", obj.scaleY);
     }
   });
-  // Move object with keys
+
   document.onkeydown = function (e) {
     let obj = canvas.getActiveObject();
     if (!obj) return true;
@@ -303,19 +285,15 @@ function CanvasGuides(canvas) {
     }
     if ($(":focus").length) return true;
     e.preventDefault();
-    // Left
     if (keyCode == 37) {
       data.left -= 1;
     }
-    // Top
     else if (keyCode == 38) {
       data.top -= 1;
     }
-    // Right
     else if (keyCode == 39) {
       data.left += 1;
     }
-    // Bottom
     else if (keyCode == 40) {
       data.top += 1;
     }
